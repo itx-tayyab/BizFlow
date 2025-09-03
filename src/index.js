f2724bd273992b75e48129f07f8e42649a8c81f8
Tayyab Tariq <tayyabtariq838@gmail.com>
2025-09-04 00:07:15 +0500

    db connected

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import dotenv from 'dotenv';
-import { app } from './app';
+import { app } from './app.js';  
+import pool from './db/db.js';
 
 dotenv.config({
-    path: './env'
+  path: "./.env",
 });
 
 const port = process.env.PORT || 5001;
 
-app.listen(port, () => {
-    console.log(`Listing to port ${port}`)
-})
+// Server running
+app.listen(port, async () => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    console.log("DB Connected. Current time:", result.rows[0]);
+  } catch (err) {
+    console.error("Database connection failed:", err.message);
+  }
+  console.log(`Server is running on port ${port}`);
+});
