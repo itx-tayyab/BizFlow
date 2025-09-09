8a231f4bae6950fdeabdaf31582d476068cf7d65
Tayyab Tariq <tayyabtariq838@gmail.com>
2025-09-09 23:46:52 +0500

    add login post method

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import {registeruser} from '../controllers/authControllers.js'
+import {registeruser,loginuser} from '../controllers/authControllers.js'
 
 const router = express.Router();
 
 router.post("/register", registeruser);
-//router.post("/login", loginuser);
+router.post("/login", loginuser);
 
 export default router;
