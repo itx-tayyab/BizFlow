a1385ed4f0394896dc31ea29d414da17fd996905
Tayyab Tariq <tayyabtariq838@gmail.com>
2025-09-09 23:52:40 +0500

    login controller added

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import {ExistingUser, CreateNewUser} from '../models/authModal.js'
+import {ExistingUser, CreateNewUser} from '../models/authModal.js';
+import {app} from '../app.js';
 
 export const registeruser = async(req,res) => {
     const {name, email, password} = req.body;
@@ -15,18 +16,6 @@ export const registeruser = async(req,res) => {
 
         const NewUser = await CreateNewUser(name, email, hashPassword);
 
-        const token = jwt.sign
-        (
-            {
-             id: NewUser.id,
-             email: NewUser.email,
-            },
-            process.env.ACCESS_TOKEN_SECRET,
-            {
-                expiresIn: process.env,ACCESS_TOKEN_SECRET_EXPIRE
-            }
-        )
-
         res.status(201).json({user: NewUser,token});
 
     } catch (err) {
@@ -35,3 +24,64 @@ export const registeruser = async(req,res) => {
     }
 }
 
+export const loginuser = async(req,res) => {
+    const {email,password} = req.body;
+
+    try {
+        const existinguser = await ExistingUser(email);
+        if(!existinguser){
+            return res.status(400).json({message: "Email Not Found"})
+        }
+
+        const isFound = await bcrypt.compare(password, existinguser.password);
+        if(!isFound){
+            return res.status(401).json({message: "Invalid Password"})
+        }
+
+        const accesstoken = jwt.sign
+        (
+            {
+             id: existinguser.id,
+             email: existinguser.email,
+            },
+            process.env.ACCESS_TOKEN_SECRET,
+            {
+                expiresIn: process.env.ACCESS_TOKEN_SECRET_EXPIRE
+            }
+        )
+
+        const refreshtoken = jwt.sign
+        (
+            {
+             id: existinguser.id,
+             email: existinguser.email,
+            },
+            process.env.REFRESH_TOKEN_SECRET,
+            {
+                expiresIn: process.env.REFRESH_TOKEN_SECRET_EXPIRE
+            }
+        )
+
+        res.cookie(
+        "jwt", refreshToken, 
+        {
+        httpOnly: true,   
+        secure: true,     
+        }
+       );
+
+       res.json({
+       message: "Login successful",
+       accesstoken,
+       user: 
+        { 
+          id: existinguser.id,
+          name: existinguser.name, 
+          email: existinguser.email 
+        },
+    });
+        
+    } catch (err) {
+        res.status(500).json({message: "Server error"});
+    }
+}
