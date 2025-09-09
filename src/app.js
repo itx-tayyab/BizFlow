dcd45f9360c4792c76693da2101c2a59d06bf99c
Tayyab Tariq <tayyabtariq838@gmail.com>
2025-09-09 23:46:19 +0500

    add routes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
+import authRoutes from "./routes/authRoutes.js";
+
 const app = express();
 
 app.use(cors({
@@ -13,5 +15,7 @@ app.use(express.static("public"))
 app.use(express.urlencoded())
 app.use(express.json())
 app.use(cookieParser())
+
+app.use("/api/auth", authRoutes);
 
 export { app }
