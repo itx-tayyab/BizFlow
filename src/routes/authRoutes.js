a79283de1e624f15a02e2823000ffc062b57ca7e
Tayyab Tariq <tayyabtariq838@gmail.com>
2025-09-11 20:10:10 +0500

    add net routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,20 @@
 import express from 'express';
-import {registeruser,loginuser} from '../controllers/authControllers.js'
+import {registeruser,loginuser, logoutuser, refreshtoken} from '../controllers/authControllers.js';
+import { authenticate } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.post("/register", registeruser);
 router.post("/login", loginuser);
+
+router.post("/logout", authenticate, logoutuser);
+router.post("/refreshtoken", refreshtoken);
+
+router.post("/authenticate", authenticate, (req, res) => {
+  // If token is valid, this will run
+  res.json({
+    message: "Token is valid ✅",
+    user: req.user
+  });});
 
 export default router;
