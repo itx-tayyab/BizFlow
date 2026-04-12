46ea97ef4034501ae37fe9dba9405e5406cad752
itx-tayyab <tayyabtariq838@gmail.com>
2026-04-12 22:27:58 +0500

    setup project to run server

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,29 @@
 import dotenv from 'dotenv';
 import { app } from './app.js';  
 import pool from './db/db.js';
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import authRoutes from './routes/authRoutes.js';
+
+const app = express();
+const port = process.env.PORT || 5001;
 
 dotenv.config({
   path: "./.env",
 });
 
-const port = process.env.PORT || 5001;
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+}))
+
+app.use(express.static("public"))
+app.use(express.urlencoded())
+app.use(express.json())
+app.use(cookieParser())
+
+app.use("/auth", authRoutes);
 
 // Server running
 app.listen(port, async () => {
@@ -18,3 +35,5 @@ app.listen(port, async () => {
   }
   console.log(`Server is running on port ${port}`);
 });
+
+export { app }
