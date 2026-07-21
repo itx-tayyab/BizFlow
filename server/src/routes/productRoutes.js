06a02837f78a67e2e63e8205c02a1e8805c74a7d
itx-tayyab <tayyabtariq838@gmail.com>
2026-07-21 23:52:57 +0500

    Implemented Product Routes with ratelimitters

--- server/src/routes/productRoutes.js
+++ server/src/routes/productRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express';
-import { addCategory, addProduct, getProducts } from '../controllers/productControllers.js';
+import { addCategory, addProduct, getCategories, getProducts } from '../controllers/productControllers.js';
 import { authenticate } from '../middlewares/authMiddleware.js';
-import { categoryLimiter, addProductLimiter, getProductsLimiter } from '../middlewares/ratelimitterMiddleware.js';
+import { categoryLimiter, addProductLimiter, getProductsLimiter, getCategoriesLimiter } from '../middlewares/ratelimitterMiddleware.js';
 
 const router = express.Router();
 
@@ -9,5 +9,8 @@ router.post("/addcategory", authenticate, categoryLimiter, addCategory);
 router.post("/addproduct", authenticate, addProductLimiter, addProduct);
 
 router.get("/getproducts", authenticate, getProductsLimiter, getProducts);
+router.get("/getcategories", authenticate, getCategoriesLimiter, getCategories);
+
+//router.delete("/deleteproduct/:id", authenticate, deleteProduct);
 
 export default router;
